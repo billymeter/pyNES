bcae66f539643d023a8bab6f2731d891544b675f
Brad Trotter <btrotter@mail.usf.edu>
2014-03-12 13:28:46 -0400

    put the code in the wrong file like a n00b

--- nes.py
+++ nes.py
@@ -9,14 +9,3 @@ import ppu
 class NES:
     # NES memory bank goes from 0x0 to 0xFFFF
     memory = bytearray(0x10000)
-    # program counter
-    pc = 0x0
-    # stack pointer
-    sp = 0x10
-    # flag register
-    p = 0x0
-
-    # general purpose registers
-    a = 0x0
-    x = 0x0
-    y = 0x0
