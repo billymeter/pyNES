ef9d4b1b0dd0a1054f68a757ca170a9c604318dc
Brad Trotter <btrotter@mail.usf.edu>
2014-05-01 14:57:40 -0400

    Cleaned up repository

--- start.py
+++
@@ -1,8 +0,0 @@
-import nes
-
-with open("roms/rom_singles/1-cli_latency.nes", 'rb') as rom:
-    emulator = nes.NES(rom.read())
-    #emulator.cpu.registers['pc'].write(0xc000)
-
-while True:
-    emulator.cpu.execute(True)
